257c1d7971a35c0c877940e12c965db237cdeb31
Michal Yashpe <michal@yashpe.com>
2014-01-19 22:03:59 +0200

    show images from a drive folder with specific id (hard-coded)

--- nagarut.js
+++ nagarut.js
@@ -1,50 +1,50 @@
 $(function(){
 
-        setTimeout(function(){
-        	$("#wrapper").CSSAnimate({marginLeft:-450, background:"rgba(255, 192, 0, 0.8)"},200);
-            $("body").css({overflow:"hidden"});
-        },3000);
+		setTimeout(function(){
+			$("#wrapper").CSSAnimate({marginLeft:-450, background:"rgba(255, 192, 0, 0.8)"},200);
+			$("body").css({overflow:"hidden"});
+		},3000);
 
-        $("#wrapper").on("mouseenter",function(){
-            $(this).CSSAnimate({marginLeft:0, background:"rgba(232, 232, 232, .9)"},200);
-            $("body").css({overflow:"auto"});
-        }).on("mouseleave",function(){
-                    $(this).CSSAnimate({marginLeft:-450, background:"rgba(255, 192, 0, 0.8)"},200);
-                    $("body").css({overflow:"hidden"});
-                });
+		$("#wrapper").on("mouseenter",function(){
+			$(this).CSSAnimate({marginLeft:0, background:"rgba(232, 232, 232, .9)"},200);
+			$("body").css({overflow:"auto"});
+		}).on("mouseleave",function(){
+					$(this).CSSAnimate({marginLeft:-450, background:"rgba(255, 192, 0, 0.8)"},200);
+					$("body").css({overflow:"hidden"});
+				});
 
-        $.mbBgndGallery.buildGallery({
-            containment:"body",
-            timer:2000,
-            effTimer:2000,
-            controls:"#controls",
-            grayScale:false,
-            shuffle:true,
-            preserveWidth:false,
-            effect:"blur",
-            // thumbs:{folderPath:"thumbs/", placeholder:"#thumbnails"},
+		$.mbBgndGallery.buildGallery({
+			containment:"body",
+			timer:2000,
+			effTimer:2000,
+			controls:"#controls",
+			grayScale:false,
+			shuffle:true,
+			preserveWidth:false,
+			effect:"blur",
+			// thumbs:{folderPath:"thumbs/", placeholder:"#thumbnails"},
 
-//              effect:{enter:{transform:"scale("+(1+ Math.random()*2)+")",opacity:0},exit:{transform:"scale("+(Math.random()*2)+")",opacity:0}},
+//			  effect:{enter:{transform:"scale("+(1+ Math.random()*2)+")",opacity:0},exit:{transform:"scale("+(Math.random()*2)+")",opacity:0}},
 
-            // If your server allow directory listing you can use:
-            // (however this doesn't work locally on your computer)
+			// If your server allow directory listing you can use:
+			// (however this doesn't work locally on your computer)
 
-//            folderPath:"../imgaes/",
+//			folderPath:"../imgaes/",
 
-            // else:
+			// else:
 
-            images:[
-            	"https://googledrive.com/host/0B-vyvs8oIgKXQTlZbkl0R0lGOXM/brain.png",
-                "http://images4.fanpop.com/image/photos/14700000/So-cute-puppies-14749028-1600-1200.jpg",
-                "http://digntaswpp.com/wp-content/uploads/2013/10/Cute-Cat-Wallpaper-HD.jpg"
-            ],
+			images:[
+				// "https://googledrive.com/host/0B-vyvs8oIgKXQTlZbkl0R0lGOXM/brain.png",
+				"http://images4.fanpop.com/image/photos/14700000/So-cute-puppies-14749028-1600-1200.jpg",
+				"http://digntaswpp.com/wp-content/uploads/2013/10/Cute-Cat-Wallpaper-HD.jpg"
+			],
 
-            onStart:function(){},
-            onPause:function(){},
-            onPlay:function(opt){},
-            onChange:function(opt,idx){},
-            onNext:function(opt){},
-            onPrev:function(opt){}
-        });
+			onStart:function(){},
+			onPause:function(){},
+			onPlay:function(opt){},
+			onChange:function(opt,idx){},
+			onNext:function(opt){},
+			onPrev:function(opt){}
+		});
 
-    });
+	});
--- sample.js
+++ sample.js
@@ -1,60 +1,68 @@
-var clientId = '*********.apps.googleusercontent.com';
-var apiKey = '##########';
+var clientId = '696513917405-tatjerfs329qthdg79s2ngscikok4k63.apps.googleusercontent.com';
+var apiKey = 'AIzaSyAE29U9mj4Wq_5iW3128Ig0irnqLdzcytE';
 var scopes = 'https://www.googleapis.com/auth/drive';
 
 
 function handleClientLoad() {
-    gapi.client.setApiKey(apiKey);
-    window.setTimeout(checkAuth,1);
+	gapi.client.setApiKey(apiKey);
+	window.setTimeout(checkAuth,1);
 }
 
 function checkAuth() {
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},handleAuthResult);
+	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},handleAuthResult);
 }
 
 function handleAuthResult(authResult) {
-    var authorizeButton = document.getElementById('authorize-button');
+	var authorizeButton = document.getElementById('authorize-button');
 
-    if (authResult && !authResult.error) {
-        authorizeButton.style.visibility = 'hidden';
-        makeApiCall();
-    }  
-    else {
-        authorizeButton.style.visibility = '';
-        authorizeButton.onclick = handleAuthClick;
-    }
+	if (authResult && !authResult.error) {
+		authorizeButton.style.visibility = 'hidden';
+		makeApiCall();
+	}  
+	else {
+		authorizeButton.style.visibility = '';
+		authorizeButton.onclick = handleAuthClick;
+	}
 }
 
 function handleAuthClick(event) {
-    gapi.auth.authorize({client_id: clientId, scope: [scopes], immediate: false}, handleAuthResult);
-    return false;
+	gapi.auth.authorize({client_id: clientId, scope: [scopes], immediate: false}, handleAuthResult);
+	return false;
 }
 
 function makeApiCall() {  
-    gapi.client.load('drive', 'v2', makeRequest);   
+	gapi.client.load('drive', 'v2', makeRequest);   
 }
 
 function makeRequest()
 {
-    // var request = gapi.client.drive.files.list({'maxResults': 5 });
+	// var request = gapi.client.drive.files.list({'maxResults': 5 });
+	var folder_id = '0B-vyvs8oIgKXelNOdnNFOHZoVTQ'
+	var request = gapi.client.request({
+		'path': "/drive/v2/files?q='"+ folder_id + "' in parents",
+		'method': 'GET',
+		'params': {'maxResults': '10'}
+		});
+	
+	request.execute(function(resp) {		  
+		for (i=0; i<resp.items.length; i++) {
+			var titulo = resp.items[i].title;
+			var fechaUpd = resp.items[i].modifiedDate;
+			var userUpd = resp.items[i].lastModifyingUserName;
+			var userEmbed = resp.items[i].embedLink;
+			var userAltLink = resp.items[i].alternateLink;
+			var selfLink = resp.items[i].selfLink;
+			var alternateLink = resp.items[i].alternateLink;
+			console.log(resp);
+			var link = "https://googledrive.com/host/" + folder_id +"/" + titulo;
 
-    var request = gapi.client.request({
-        'path': '/drive/v2/files',
-        'method': 'GET',
-        'params': {'maxResults': '1'}
-        });
-    
-    request.execute(function(resp) {          
-        for (i=0; i<resp.items.length; i++) {
-            var titulo = resp.items[i].title;
-            var fechaUpd = resp.items[i].modifiedDate;
-            var userUpd = resp.items[i].lastModifyingUserName;
-            var userEmbed = resp.items[i].embedLink;
-            var userAltLink = resp.items[i].alternateLink;
+			var fileInfo = document.createElement('li');
+			fileInfo.appendChild(document.createTextNode('TITLE: ' + titulo + ' - LAST MODIF: ' + fechaUpd + ' - BY: ' + userUpd ));
+			document.getElementById('content').appendChild(fileInfo);
+			// document.getElementById('content').insertAdjacentHTML('beforeend', '<img width="100" src="'+ link + '"/>');
 
-            var fileInfo = document.createElement('li');
-            fileInfo.appendChild(document.createTextNode('TITLE: ' + titulo + ' - LAST MODIF: ' + fechaUpd + ' - BY: ' + userUpd ));                
-            document.getElementById('content').appendChild(fileInfo);
-        }
-    });    
+			var html = '<iframe src="http://docs.google.com/gview?url="'+alternateLink+'"&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>';
+			document.getElementById('content').insertAdjacentHTML('beforeend', html);
+		}
+	});	
 }
